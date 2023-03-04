baa4f82ab094a3b9862d54065fd2522b63c00775
‘Autism-sow <l>
2023-03-04 22:48:58 +0800

    初始化JVM项目

---
+++ chapter01/src/com/autism/java/StackStruTest.java
@@ -0,0 +1,15 @@
+package com.autism.java;
+
+/**
+ * github：<a href="https://github.com/Autism-sow">小肖coding</a><br>
+ *
+ * @author 小肖coding
+ * 创建时间：2023/3/4 22:48
+ * @version v1.0.0
+ * @description:
+ */
+public class StackStruTest {
+    public static void main(String[] args) {
+
+    }
+}
